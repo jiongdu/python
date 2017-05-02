ed908511a25bd866df46d32308548cbd0b7158c4
jiongdu <dujiong.uestc@gmail.com>
2017-05-03 00:50:20 +0800

    "add xpath&css selector"

--- ArticleSpider/ArticleSpider/spiders/jobbole.py
+++ ArticleSpider/ArticleSpider/spiders/jobbole.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class JobboleSpider(scrapy.Spider):
     name = "jobbole"
@@ -8,5 +8,45 @@ class JobboleSpider(scrapy.Spider):
     start_urls = ['http://blog.jobbole.com/111038']
 
     def parse(self, response):
-        selector = response.xpath('//*[@id="post-111038"]/div[1]/h1/text()')
+
+#use xpath
+        #selector = response.xpath("/html/body/div[1]/div[3]/div[1]/div[1]/h1/text()")
+        #selector = response.xpath('//div[@class="entry-header"]/h1/text()')
+        title = response.xpath('//*[@id="post-111038"]/div[1]/h1/text()').extract()[0]
+        create_date = response.xpath("//p[@class='entry-meta-hide-on-mobile']/text()").extract()[0].\
+            strip().replace("·","").strip()
+        praise_nums = response.xpath("//span[contains(@class, 'vote-post-up')]/h10/text()").extract()[0]
+        fav_nums = response.xpath("//span[contains(@class, 'bookmark-btn')]/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = match_re.group(1)
+        comment_nums = response.xpath("//a[@href='#article-comment']/span/text()").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = match_re.group(1)
+        content = response.xpath("//div[@class='entry']").extract()[0]
+        tag_list = response.xpath("//p[@class='entry-meta-hide-on-mobile']/a/text()").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
+
+#use css selector
+        title = response.css(".entry-header h1::text").extract()[0]
+        create_date = response.css("p.entry-meta-hide-on-mobile::text").extract()[0].strip().replace("·","").strip()
+        praise_nums = response.css(".vote-post-up h10::text").extract()[0]
+        fav_nums = response.css(".bookmark-btn::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", fav_nums)
+        if match_re:
+            fav_nums = int(match_re.group(1))
+        else:
+            fav_nums = 0
+        comment_nums = response.css("a[href='#article-comment'] span::text").extract()[0]
+        match_re = re.match(".*?(\d+).*", comment_nums)
+        if match_re:
+            comment_nums = int(match_re.group(1))
+        else:
+            comment_nums = 0
+        content = response.css("div.entry").extract()[0]
+        tag_list = response.css("p.entry-meta-hide-on-mobile a::text").extract()
+        tag_list = [element for element in tag_list if not element.strip().endswith("评论")]
+        tags = ",".join(tag_list)
         pass
